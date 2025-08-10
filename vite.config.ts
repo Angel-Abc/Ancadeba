a65ebb44272d4cf58794c6dd7ac85c362b41e041
AngelAbc <AngelAbc@AngelAbc.com>
2025-08-10 21:34:17 +0200

    Make the game json files available under the /data/ url

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { resolve } from 'node:path'
+
+const gameFolder = process.env.GAME_FOLDER || 'sample-game'
+const rootDir = fileURLToPath(new URL('.', import.meta.url))
+const withEditor = process.env.WITH_EDITOR !== 'false'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: `${gameFolder}/**/*`, dest: 'data' },
+      ],
+      watch: {
+        reloadPageOnChange: true,
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@loader': fileURLToPath(new URL('./engine/loader', import.meta.url)),
@@ -13,5 +29,14 @@ export default defineConfig({
       '@engine': fileURLToPath(new URL('./engine/engine', import.meta.url)),
       '@editor': fileURLToPath(new URL('./editor', import.meta.url)),
     }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(rootDir, 'index.html'),
+        ...(withEditor ? { editor: resolve(rootDir, 'editor/editor.html') } : {}),
+      },
+    },
   }
+
 })
