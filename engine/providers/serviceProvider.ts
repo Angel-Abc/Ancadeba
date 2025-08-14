1756f54acde6803c1d8bb9df7a30dc3feb6b28b4
Angel-Abc <157061520+Angel-Abc@users.noreply.github.com>
2025-08-14 11:04:09 +0200

    docs: add service provider JSDoc

--- engine/providers/serviceProvider.ts
+++ engine/providers/serviceProvider.ts
@@ -1,15 +1,36 @@
 import { token, Token } from '@ioc/token'
 import { Container } from '@ioc/types'
 
+/**
+ * Defines a contract for resolving services by their {@link Token}.
+ */
 export interface IServiceProvider {
+    /**
+     * Retrieve a service instance associated with the given token.
+     *
+     * @param t - Token identifying the desired service.
+     * @returns The resolved service instance.
+     */
     getService<T>(t: Token<T>): T
 }
 
 const logName = 'ServiceProvider'
 export const serviceProviderToken = token<IServiceProvider>(logName)
 export class ServiceProvider implements IServiceProvider {
+    /**
+     * Creates a new instance of {@link ServiceProvider} that resolves
+     * services from the specified IoC container.
+     *
+     * @param container - Container used to resolve services.
+     */
     constructor(private container: Container){}
 
+    /**
+     * Resolve a service instance for the provided token.
+     *
+     * @param t - Token identifying the service to resolve.
+     * @returns The resolved service instance.
+     */
     public getService<T>(t: Token<T>): T {
         return this.container.resolve(t)
     }
