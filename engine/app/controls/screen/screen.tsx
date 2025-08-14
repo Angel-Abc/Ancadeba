9e31fea50a52072c8c822adaeb4712c44970b08c
Angel-Abc <157061520+Angel-Abc@users.noreply.github.com>
2025-08-14 08:35:03 +0200

    Handle unsupported screen types

--- engine/app/controls/screen/screen.tsx
+++ engine/app/controls/screen/screen.tsx
@@ -1,15 +1,21 @@
 import { Screen as ScreenData } from '@loader/data/page'
 import { Grid } from './grid'
+import { logWarning } from '@utils/logMessage'
 
 interface ScreenProps {
     screen: ScreenData
 }
 
-export const Screen: React.FC<ScreenProps> = ({ screen }): React.JSX.Element => {
+const logName = 'Screen'
+
+export const Screen: React.FC<ScreenProps> = ({ screen }): React.JSX.Element | null => {
     switch (screen.type) {
         case 'grid':
             return (
                 <Grid screen={screen} />
             )
+        default:
+            logWarning(logName, 'Unsupported screen type: {0}', screen.type)
+            return null
     }
 }
