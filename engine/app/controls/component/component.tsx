3c606467bc2ea4219f82554aca92d09648b4bfdb
Angel-Abc <157061520+Angel-Abc@users.noreply.github.com>
2025-08-14 08:33:22 +0200

    feat: add fallback for unknown component types

--- engine/app/controls/component/component.tsx
+++ engine/app/controls/component/component.tsx
@@ -1,13 +1,15 @@
 import { Component as ComponentData } from '@loader/data/component'
 import { componentRegistry } from './componentRegistry'
+import { logWarning } from '@utils/logMessage'
 
 interface ComponentProps {
     component: ComponentData
 }
 
 const DefaultComponent: React.FC<ComponentProps> = ({ component }): React.JSX.Element => {
+    logWarning('Component', 'Unknown component type: {0}', component.type)
     return (
-        <div>TODO: {component.type}</div>
+        <div>Unsupported component type: {component.type}</div>
     )
 }
 
