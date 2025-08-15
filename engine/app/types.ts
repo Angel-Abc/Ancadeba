a5b20a254a6f41672ebabf637f3939b46a25b656
Angel-Abc <157061520+Angel-Abc@users.noreply.github.com>
2025-08-15 20:50:40 +0200

    docs: explain CSS custom properties type

--- engine/app/types.ts
+++ engine/app/types.ts
@@ -1,3 +1,7 @@
+/**
+ * Extends React.CSSProperties to include support for CSS custom properties.
+ * Allows defining style attributes with `--` prefixes to set CSS variables.
+ */
 export type CSSCustomProperties = React.CSSProperties & {
   [key: `--${string}`]: string
 }
