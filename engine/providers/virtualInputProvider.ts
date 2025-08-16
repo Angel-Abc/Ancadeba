7c113f5dfdf8c3e48df8a9adfa398e801dd94228
Angel-Abc <157061520+Angel-Abc@users.noreply.github.com>
2025-08-16 13:31:00 +0200

    refactor: enforce camel case and const

--- engine/providers/virtualInputProvider.ts
+++ engine/providers/virtualInputProvider.ts
@@ -28,11 +28,11 @@ export const virtualInputProviderDependencies: Token<unknown>[] = [
     gameDataProviderToken
 ]
 export class VirtualInputProvider implements IVirtualInputProvider {
-    private CleanUpFn: CleanUp | null = null
+    private cleanupFn: CleanUp | null = null
 
     constructor(
         private virtualInputsLoader: IVirtualInputsLoader,
-        private messagebus: IMessageBus,
+        private messageBus: IMessageBus,
         private gameDataProvider: IGameDataProvider
     ) {}
 
@@ -51,11 +51,11 @@ export class VirtualInputProvider implements IVirtualInputProvider {
      */
     public async initialize(): Promise<void> {
         if (this.gameDataProvider.Game.loadedVirtualInputs.size === 0) await this.loadVirtualInputs()
-        this.CleanUpFn = this.messagebus.registerMessageListener(
+        this.cleanupFn = this.messageBus.registerMessageListener(
             VIRTUAL_KEY,
             message => {
                 if (message.payload && this.gameDataProvider.Game.loadedVirtualInputs.has(message.payload as string)) {
-                    this.messagebus.postMessage({
+                    this.messageBus.postMessage({
                         message: VIRTUAL_INPUT,
                         payload: this.gameDataProvider.Game.loadedVirtualInputs.get(message.payload as string)?.virtualInput
                     })
@@ -74,8 +74,8 @@ export class VirtualInputProvider implements IVirtualInputProvider {
      *   duplicate registrations.
      */
     public cleanup(): void {
-        this.CleanUpFn?.()
-        this.CleanUpFn = null
+        this.cleanupFn?.()
+        this.cleanupFn = null
     }
 
     /**
@@ -84,7 +84,7 @@ export class VirtualInputProvider implements IVirtualInputProvider {
      * game data provider's state.
      */
     private async loadVirtualInputs(): Promise<void> {
-        var inputs = await this.virtualInputsLoader.loadVirtualInputs(this.gameDataProvider.Game.game.virtualInputs)
+        const inputs = await this.virtualInputsLoader.loadVirtualInputs(this.gameDataProvider.Game.game.virtualInputs)
         inputs.forEach(input => {
             input.virtualKeys.forEach(virtualKey => this.gameDataProvider.Game.loadedVirtualInputs.set(virtualKey, input))
         })
