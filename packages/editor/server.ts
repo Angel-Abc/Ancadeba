021ec371dc33939a4506d67aeb087c910444f820
Angel-Abc <157061520+Angel-Abc@users.noreply.github.com>
2025-08-20 01:44:04 +0200

    feat(editor): enable cors middleware (#207)

--- packages/editor/server.ts
+++ packages/editor/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import { promises as fs } from 'fs'
 import path from 'path'
 
@@ -7,6 +8,7 @@ const resolvedBase = path.resolve(basePath)
 
 const app = express()
 app.use(express.json())
+app.use(cors({ origin: 'http://localhost:5174' }))
 
 const safeJoin = (subPath: string): string => {
   const filePath = path.resolve(resolvedBase, subPath)
