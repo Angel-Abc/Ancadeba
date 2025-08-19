31c1cc4763f5df63f764b4a37188090b8e636a69
Angel-Abc <157061520+Angel-Abc@users.noreply.github.com>
2025-08-19 10:13:56 +0200

    chore: separate editor vite config

---
+++ editor/vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
+import { resolve } from 'node:path'
+
+const rootDir = fileURLToPath(new URL('.', import.meta.url))
+
+export default defineConfig({
+  root: rootDir,
+  envPrefix: ['VITE_', 'LOG_', 'GAME_'],
+  plugins: [
+    react({
+      jsxRuntime: 'automatic',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@utils': fileURLToPath(new URL('../utils', import.meta.url)),
+      '@ioc': fileURLToPath(new URL('../engine/ioc', import.meta.url)),
+      '@loader/schema': fileURLToPath(new URL('../engine/loader/schema', import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: resolve(rootDir, 'editor.html'),
+    },
+  },
+  server: {
+    port: 5174,
+  },
+})
