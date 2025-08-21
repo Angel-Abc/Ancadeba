ee17619caa30b7461d3ea6471128fb05365290d7
AngelAbc <AngelAbc@AngelAbc.com>
2025-08-21 17:29:21 +0200

    Styled the tree a bit

--- packages/editor/app/treeNode.tsx
+++ packages/editor/app/treeNode.tsx
@@ -1,13 +1,20 @@
 import { GameItemTreeNode } from '@editor/providers/editTreeProvider'
+import { AiTwotoneFolder, AiTwotoneFolderOpen } from 'react-icons/ai'
 
 interface TreeNodeProps {
     node: GameItemTreeNode
 }
 
 export const TreeNode: React.FC<TreeNodeProps> = ({ node }): React.JSX.Element => {
+    const icon = node.isCollapsed ? <AiTwotoneFolder /> : <AiTwotoneFolderOpen />
     return (
-        <ul>{node.label}
-            {node.children.map(child => <li><TreeNode node={child} /></li>)}
-        </ul>
+        <div>
+            <label>{icon} {node.label}</label>
+            {!node.isCollapsed && node.children.length > 0 && (
+                <div className='children'>
+                    {node.children.map(child => <TreeNode node={child} />)}
+                </div>
+            )}
+        </div>
     )
 }
