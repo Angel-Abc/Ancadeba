bafa6904218f9c68f9f978752e07ad936f71022f
Angela-BC <angelabc17@outlook.com>
2026-03-20 17:33:28 +0100

    Fixed a duplicate IoC registration. Updated pnpm version.

--- apps/game-client/src/helpers/iocHelper.ts
+++ apps/game-client/src/helpers/iocHelper.ts
@@ -1,9 +1,5 @@
-import {
-  registerServices as registerEngineUiServices,
-} from '@ancadeba/engine-ui'
 import type { IRegistrar } from '@ancadeba/utils'
 
 export function registerServices(container: IRegistrar): void {
   container.registerAll([])
-  registerEngineUiServices(container)
 }
